f1138a6e70f6bef4519f342e48a84fbd2b139f48
CR11MP <103106288+CR11MP@users.noreply.github.com>
2022-09-29 16:40:58 +1200

    kekw

--- src/main/java/me/crimp/claudius/mod/modules/render/DeathEffect.java
+++ src/main/java/me/crimp/claudius/mod/modules/render/DeathEffect.java
@@ -1,23 +1,15 @@
 package me.crimp.claudius.mod.modules.render;
 
-import me.crimp.claudius.managers.ColorManager;
 import me.crimp.claudius.mod.modules.Module;
-import me.crimp.claudius.mod.modules.pvp.PopCounter;
 import me.crimp.claudius.mod.setting.Setting;
-import me.crimp.claudius.utils.RenderUtil;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.util.math.Vec3d;
-
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
 
 
 public class DeathEffect extends Module {
     private static DeathEffect INSTANCE = new DeathEffect();
     private final Setting<Boolean> sound = register(new Setting<>("sound", true, ""));
-    private final Setting<Integer> numberSound = register(new Setting<>("numberSound", 1, 1, 10));
 
     public DeathEffect() {
         super("DeathEffect", "DeathEffect", Category.RENDER, true, false, false);
@@ -29,16 +21,6 @@ public class DeathEffect extends Module {
         }
         return INSTANCE;
     }
-
-    final Object sync = new Object();
-
-    @Override
-    public void onUpdate() {
-        if (mc.world == null) {
-            return;
-        }
-    }
-
     public void onDeath(EntityPlayer player) {
         mc.world.spawnEntity(new EntityLightningBolt(mc.world, player.posX, player.posY, player.posZ, true));
         if (this.sound.getValue()) {
