066cd326555588e25c9d741553cf1111d0fcd503
CR11MP <103106288+CR11MP@users.noreply.github.com>
2022-10-07 01:52:29 +1200

    AntiChainPop

--- src/main/java/me/crimp/claudius/mod/modules/misc/AntiChainPop.java
+++ src/main/java/me/crimp/claudius/mod/modules/misc/AntiChainPop.java
@@ -1,6 +1,9 @@
 package me.crimp.claudius.mod.modules.misc;
 
 import me.crimp.claudius.mod.modules.Module;
+import net.minecraft.network.play.client.CPacketChatMessage;
+import net.minecraft.network.play.server.SPacketDisconnect;
+import net.minecraft.util.text.TextComponentString;
 
 public class AntiChainPop extends Module {
     public AntiChainPop() {
@@ -9,6 +12,9 @@ public class AntiChainPop extends Module {
 
     @Override
     public void onUpdate() {
-        if (mc.player.getHealth() == 3.5f) {mc.world.sendQuittingDisconnectingPacket();}
+        if (mc.player.getHealth() == 3.5f) {
+            mc.player.connection.sendPacket(new CPacketChatMessage("Holdon Mum Wants Me"));
+            mc.player.connection.sendPacket(new SPacketDisconnect(new TextComponentString("ANTICHAINPOP")));
+        }
     }
 }
