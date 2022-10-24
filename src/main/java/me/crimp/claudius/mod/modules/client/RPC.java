bcd5c48dfdb26af629f5279a54058827f3e2f257
CR11MP <103106288+CR11MP@users.noreply.github.com>
2022-10-25 01:37:31 +1200

    Tons of work
    Signed-off-by: CR11MP <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mod/modules/client/RPC.java
+++ src/main/java/me/crimp/claudius/mod/modules/client/RPC.java
@@ -7,7 +7,7 @@ import me.crimp.claudius.mod.setting.Setting;
 public class RPC
         extends Module {
     public static RPC INSTANCE;
-    public final Setting<Boolean> IP = this.register(new Setting<>("id",true));
+    //public final Setting<Boolean> IP = this.register(new Setting<>("id",true));
 
     public RPC() {
         super("DiscordRichP", "Discord rich presence", Module.Category.CLIENT, false, false, false);
