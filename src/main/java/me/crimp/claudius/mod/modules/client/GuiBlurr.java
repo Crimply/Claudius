64743e7830c7fc5e4eb59d3f70a80b30a4e87295
CR11MP <103106288+CR11MP@users.noreply.github.com>
2022-10-26 17:06:06 +1200

    Tons of work
    Signed-off-by: CR11MP <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mod/modules/client/GuiBlurr.java
+++ src/main/java/me/crimp/claudius/mod/modules/client/GuiBlurr.java
@@ -6,12 +6,9 @@ import net.minecraft.client.*;
 import net.minecraft.client.gui.inventory.*;
 import net.minecraft.client.gui.*;
 import net.minecraftforge.fml.client.*;
-import net.minecraft.client.renderer.*;
-import net.minecraft.entity.player.*;
 import net.minecraft.util.*;
 
-public class GuiBlurr extends Module
-{
+public class GuiBlurr extends Module {
     final Minecraft mc;
 
     public GuiBlurr() {
@@ -19,33 +16,19 @@ public class GuiBlurr extends Module
         this.mc = Minecraft.getMinecraft();
     }
 
-    public void onDisable() {
+    @Override
+    public void onEnable() {
         if (this.mc.world != null) {
-            Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
-        }
-    }
-
-    public void onUpdate() {
-        if (this.mc.world != null) {
-            if (ClickGuiModule.INSTANCE.isEnabled() || this.mc.currentScreen instanceof GuiContainer || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiConfirmOpenLink || this.mc.currentScreen instanceof GuiEditSign || this.mc.currentScreen instanceof GuiGameOver || this.mc.currentScreen instanceof GuiOptions || this.mc.currentScreen instanceof GuiIngameMenu || this.mc.currentScreen instanceof GuiVideoSettings || this.mc.currentScreen instanceof GuiScreenOptionsSounds || this.mc.currentScreen instanceof GuiControls || this.mc.currentScreen instanceof GuiCustomizeSkin || this.mc.currentScreen instanceof GuiModList) {
-                if (OpenGlHelper.shadersSupported && Util.mc.getRenderViewEntity() instanceof EntityPlayer) {
-                    if (Util.mc.entityRenderer.getShaderGroup() != null) {
-                        Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
-                    }
-                    try {
-                        Util.mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blurr.json"));
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                else if (Util.mc.entityRenderer.getShaderGroup() != null && Util.mc.currentScreen == null) {
-                    Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+            if (ClickGuiModule.INSTANCE.isEnabled() || this.mc.currentScreen instanceof GuiContainer || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiConfirmOpenLink || this.mc.currentScreen instanceof GuiEditSign || this.mc.currentScreen instanceof GuiGameOver || this.mc.currentScreen instanceof GuiOptions || this.mc.currentScreen instanceof GuiIngameMenu || this.mc.currentScreen instanceof GuiVideoSettings || this.mc.currentScreen instanceof GuiScreenOptionsSounds || this.mc.currentScreen instanceof GuiControls || this.mc.currentScreen instanceof GuiCustomizeSkin || this.mc.currentScreen instanceof GuiModList && this.mc.world != null ) {
+                    Util.mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blurr.json"));
                 }
             }
-            else if (Util.mc.entityRenderer.getShaderGroup() != null) {
+        }
+
+    @Override
+    public void onDisable () {
+            if (this.mc.world != null) {
                 Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
             }
         }
     }
-}
