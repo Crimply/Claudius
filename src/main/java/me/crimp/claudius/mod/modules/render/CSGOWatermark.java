c49a3fc792dd0314db4e024c2ce4a921dbc3c0ef
Crimply <103106288+CR11MP@users.noreply.github.com>
2022-11-06 20:54:55 +1200

    watermark work
    Signed-off-by: Crimply <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mod/modules/render/CSGOWatermark.java
+++ src/main/java/me/crimp/claudius/mod/modules/render/CSGOWatermark.java
@@ -6,37 +6,55 @@ import me.crimp.claudius.mod.modules.Module;
 import me.crimp.claudius.mod.setting.Setting;
 import me.crimp.claudius.utils.ColorUtil;
 import me.crimp.claudius.utils.RenderUtil;
-import me.crimp.claudius.utils.Timer;
 
 public class CSGOWatermark extends Module {
-
-    Timer delayTimer = new Timer();
-    public Setting<Integer> X = this.register(new Setting("WatermarkX", 10, 0, 1200));
-    public Setting<Integer> Y = this.register(new Setting("WatermarkY", 10, 0, 1200));
-    public float hue;
-    public int red = 1;
-    public int green = 1;
-    public int blue = 1;
+    public Setting<Integer> X = this.register(new Setting("X", 10, 0, 1200));
+    public Setting<Integer> Y = this.register(new Setting("Y", 10, 0, 1200));
+    public Setting<Boolean> Name = this.register(new Setting("Name", true));
+    public Setting<Boolean> Ping = this.register(new Setting("Ping", true));
+    public Setting<Boolean> Config = this.register(new Setting("Config", false));
+    public Setting<Boolean> Fps = this.register(new Setting("Fps", false));
+    public Setting<Boolean> Ip = this.register(new Setting("Config", false));
 
     private String message = "";
+
     public CSGOWatermark() {
         super("CSGOWatermark", "CSGOey", Category.Render, true, false, false);
     }
 
     @Override
-    public void onRender2D ( Render2DEvent event ) {
+    public void onRender2D(Render2DEvent event) {
         drawCsgoWatermark();
     }
 
-    public void drawCsgoWatermark () {
-        int padding = 5;
-        message = "Claudius | " + mc.player.getName() + " | " + Claudius.serverManager.getPing() + "ms";
-        Integer textWidth = mc.fontRenderer.getStringWidth(message); // taken from wurst+ 3
-        Integer textHeight = mc.fontRenderer.FONT_HEIGHT; // taken from wurst+ 3
+    public void drawCsgoWatermark() {
+        message = "Claudius ";
+        if (Name.getValue()) {
+            message = message + mc.player.getName();
+        }
+        if (Ping.getValue()) {
+            message = message + " " + Claudius.serverManager.getPing() + "ms";
+        }
+        if (Config.getValue()) {
+            message = message + " " + Claudius.configManager.config;
+        }
+        if (Fps.getValue()) {
+            message = message + " " + mc.fpsCounter;
+        }
+        if (Ip.getValue()) {
+            if (!mc.isSingleplayer()) {
+                String serverIp = mc.currentServerData.serverIP;
+                message = message + " " + serverIp + ":" + mc.serverPort;
+            } else {
+                message = message + " Singleplayer";
+            }
 
-        //RenderUtil.drawRectangleCorrectly(X.getValue() - 4, Y.getValue() - 4, textWidth + 14, textHeight + 10, ColorUtil.toRGBA(22, 22, 22, 255));
-        RenderUtil.drawRectangleCorrectly(X.getValue(), Y.getValue(), textWidth + 4, textHeight + 4, ColorUtil.toRGBA(0, 0, 0, 255));
-        RenderUtil.drawRectangleCorrectly(X.getValue(), Y.getValue(), textWidth + 8, textHeight + 4, ColorUtil.toRGBA(0, 0, 0, 255)); // inside
-        Claudius.textManager.drawString(message, X.getValue() + 3, Y.getValue() + 3, ColorUtil.toRGBA(255, 255, 255, 255), false); // text
+            Integer Width = mc.fontRenderer.getStringWidth(message); // taken from wurst+ 3
+            Integer Height = mc.fontRenderer.FONT_HEIGHT; // taken from wurst+ 3
+
+
+            RenderUtil.drawRectangleCorrectly(X.getValue(), Y.getValue(), Width + 8, Height + 4, ColorUtil.toRGBA(0, 0, 0, 255)); // inside
+            Claudius.textManager.drawString(message, X.getValue() + 3, Y.getValue() + 2, ColorUtil.toRGBA(255, 255, 255, 255), false); // text
+        }
     }
 }
