47fca00d1a7dc3929b6d6fe0852eaa8c5a819356
Crimply <103106288+CR11MP@users.noreply.github.com>
2022-11-06 19:57:59 +1200

    dev cape
    Signed-off-by: Crimply <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mod/modules/text/SelfPopCounter.java
+++ src/main/java/me/crimp/claudius/mod/modules/text/SelfPopCounter.java
@@ -7,6 +7,7 @@ import me.crimp.claudius.event.events.TotemPopEvent;
 import me.crimp.claudius.mod.command.Command;
 import me.crimp.claudius.mod.modules.Module;
 import me.crimp.claudius.mod.modules.client.HUD;
+import me.crimp.claudius.utils.Timer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -19,28 +20,32 @@ public class SelfPopCounter extends Module {
     public SelfPopCounter() {
         super("SelfPopCounter", "Counts self totem pops.", Category.Text, true, true, true);
     }
+
     public static SelfPopCounter INSTANCE = new SelfPopCounter();
-    private final Map<EntityPlayer, Integer> pops = new ConcurrentHashMap<>();
-    private final Set<EntityPlayer> announcers = new ConcurrentSet<>();
     String totemSpelling;
     public int l_Count;
+    private Boolean hasdied = false;
 
 
     @SubscribeEvent
     public void onPop(TotemPopEvent event) {
         if (!HUD.INSTANCE.SelfPops.getValue()) return;
+        if  (hasdied.equals(true)) {l_Count = 1;}
         l_Count++;
         if (l_Count == 1) {totemSpelling = " totem";}
-        else if (l_Count > 1) {totemSpelling = " totems";}
-        if (event.getPlayer().getEntityId() == mc.player.getEntityId()) {Command.sendOverwriteClientMessage(ChatFormatting.BLUE + "You" + ChatFormatting.RESET + " have popped " + ChatFormatting.BLUE + l_Count + ChatFormatting.RESET + totemSpelling);}
+        else if (l_Count > 1 || l_Count == 0) {totemSpelling = " totems";}
+        if (event.getPlayer().getEntityId() == mc.player.getEntityId()) {
+            Command.sendOverwriteClientMessage(ChatFormatting.BLUE + "You" + ChatFormatting.RESET + " have popped " + ChatFormatting.BLUE + l_Count + ChatFormatting.RESET + totemSpelling);}
     }
 
 
     @SubscribeEvent
     public void onDeath(DeathEvent event) {
         if (!HUD.INSTANCE.SelfPops.getValue()) return;
-        if (event.getPlayer().getEntityId() == mc.player.getEntityId()) {Command.sendOverwriteClientMessage(ChatFormatting.BLUE + "You" + ChatFormatting.RESET + " just died after popping " + ChatFormatting.BLUE + l_Count + ChatFormatting.RESET + totemSpelling);}
-        if (event.getPlayer().getEntityId() == mc.player.getEntityId()) {l_Count = 0;}
+        if (event.getPlayer().getEntityId() == mc.player.getEntityId()) {
+            hasdied = true;
+            Command.sendOverwriteClientMessage(ChatFormatting.BLUE + "You" + ChatFormatting.RESET + " just died after popping " + ChatFormatting.BLUE + l_Count + ChatFormatting.RESET + totemSpelling);
+            }
+        }
     }
-}
 
