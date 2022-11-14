f2ceb9aafed3219905a81baef36c72592c87b0db
Crimply <103106288+CR11MP@users.noreply.github.com>
2022-11-14 18:54:57 +1200

    V1.6.1
    Signed-off-by: Crimply <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mod/modules/render/SwingSpeed.java
+++ src/main/java/me/crimp/claudius/mod/modules/render/SwingSpeed.java
@@ -1,14 +1,9 @@
 package me.crimp.claudius.mod.modules.render;
 
-import me.crimp.claudius.Claudius;
-import me.crimp.claudius.mixins.mixins.EntityLivingBaseMixin;
-import me.crimp.claudius.mod.command.Command;
 import me.crimp.claudius.mod.modules.Module;
-import me.crimp.claudius.mod.setting.Setting;
 
 public class SwingSpeed extends Module {
    //  public Setting<Boolean> Ampl = this.register(new Setting<>("Amplifier", false));
-
     public SwingSpeed() {
         super("SwingSpeed", "strichcode", Category.Render, true, true, false);
     }
