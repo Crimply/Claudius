fadc347ed170cd9534e90711f71be7e003a33871
Crimply <103106288+CR11MP@users.noreply.github.com>
2022-12-06 10:49:07 +1200

    k
    Signed-off-by: Crimply <103106288+CR11MP@users.noreply.github.com>

--- src/main/java/me/crimp/claudius/mixins/mixins/MixinRenderLivingBase.java
+++ src/main/java/me/crimp/claudius/mixins/mixins/MixinRenderLivingBase.java
@@ -1,4 +1,5 @@
 package me.crimp.claudius.mixins.mixins;
+
 import me.crimp.claudius.claudius;
 import me.crimp.claudius.mod.modules.client.ClickGuiModule;
 import me.crimp.claudius.mod.modules.render.Wireframe;
@@ -38,8 +39,8 @@ public abstract class MixinRenderLivingBase<T extends EntityLivingBase> extends 
 
     protected MixinRenderLivingBase(RenderManager renderManager) {
         super(renderManager);
-        this.green = 0.0F;
         this.red = 0.0F;
+        this.green = 0.0F;
         this.blue = 0.0F;
     }
 
@@ -47,7 +48,8 @@ public abstract class MixinRenderLivingBase<T extends EntityLivingBase> extends 
      * @author
      * @reason
      */
-    
+
+
     @Overwrite
     public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
         if (!MinecraftForge.EVENT_BUS.post(new RenderLivingEvent.Pre(entity, RenderLivingBase.class.cast(this), partialTicks, x, y, z))) {
@@ -107,46 +109,18 @@ public abstract class MixinRenderLivingBase<T extends EntityLivingBase> extends 
                     if (flag1)
                         unsetScoreTeamColor();
                 } else {
-                    if (Wireframe.INSTANCE.isOn() && (Wireframe.INSTANCE).players.getValue().booleanValue() && entity instanceof EntityPlayer && (Wireframe.INSTANCE).mode.getValue().equals(Wireframe.RenderMode.SOLID)) {
-                        this.red = (ClickGuiModule.INSTANCE).red.getValue().intValue() / 255.0F;
-                        this.green = (ClickGuiModule.INSTANCE).green.getValue().intValue() / 255.0F;
-                        this.blue = (ClickGuiModule.INSTANCE).blue.getValue().intValue() / 255.0F;
-                        GlStateManager.pushMatrix();
-                        GL11.glPushAttrib(1048575);
-                        GL11.glDisable(3553);
-                        GL11.glDisable(2896);
-                        GL11.glEnable(2848);
-                        GL11.glEnable(3042);
-                        GL11.glBlendFunc(770, 771);
-                        GL11.glDisable(2929);
-                        GL11.glDepthMask(false);
-                        if (claudius.friendManager.isFriend(entity.getName()) || entity == (Minecraft.getMinecraft()).player) {
-                            GL11.glColor4f(0.0F, 191.0F, 255.0F, (Wireframe.INSTANCE).alpha.getValue().floatValue() / 255.0F);
-                        }
-                        renderModel(entity, f6, f5, f8, f2, f7, f4);
-                        GL11.glDisable(2896);
-                        GL11.glEnable(2929);
-                        GL11.glDepthMask(true);
-                        if (claudius.friendManager.isFriend(entity.getName()) || entity == (Minecraft.getMinecraft()).player) {
-                            GL11.glColor4f(0.0F, 191.0F, 255.0F, (Wireframe.INSTANCE).alpha.getValue().floatValue() / 255.0F);
-                        }
-                        renderModel(entity, f6, f5, f8, f2, f7, f4);
-                        GL11.glEnable(2896);
-                        GlStateManager.popAttrib();
-                        GlStateManager.popMatrix();
-                    }
                     boolean flag1 = setDoRenderBrightness(entity, partialTicks);
-                    if (!(entity instanceof EntityPlayer) || (Wireframe.INSTANCE.isOn() && (Wireframe.INSTANCE).mode.getValue().equals(Wireframe.RenderMode.WIREFRAME) && (Wireframe.INSTANCE).playerModel.getValue().booleanValue()) || Wireframe.INSTANCE.isOff())
+                    if (!(entity instanceof EntityPlayer) || (Wireframe.getINSTANCE().isOn() && (Wireframe.getINSTANCE()).mode.getValue().equals(Wireframe.RenderMode.WIREFRAME) && (Wireframe.getINSTANCE()).playerModel.getValue().booleanValue()) || Wireframe.getINSTANCE().isOff())
                         renderModel(entity, f6, f5, f8, f2, f7, f4);
                     if (flag1)
                         unsetBrightness();
                     GlStateManager.depthMask(true);
                     if (!(entity instanceof EntityPlayer) || !((EntityPlayer) entity).isSpectator())
                         renderLayers(entity, f6, f5, partialTicks, f8, f2, f7, f4);
-                    if (Wireframe.INSTANCE.isOn() && (Wireframe.INSTANCE).players.getValue().booleanValue() && entity instanceof EntityPlayer && (Wireframe.INSTANCE).mode.getValue().equals(Wireframe.RenderMode.WIREFRAME)) {
-                        this.red = (ClickGuiModule.INSTANCE).red.getValue().intValue() / 255.0F;
-                        this.green = (ClickGuiModule.INSTANCE).green.getValue().intValue() / 255.0F;
-                        this.blue = (ClickGuiModule.INSTANCE).blue.getValue().intValue() / 255.0F;
+                    if (Wireframe.getINSTANCE().isOn() && (Wireframe.getINSTANCE()).players.getValue().booleanValue() && entity instanceof EntityPlayer && (Wireframe.getINSTANCE()).mode.getValue().equals(Wireframe.RenderMode.WIREFRAME)) {
+                        this.red = ClickGuiModule.INSTANCE.red.getValue().intValue() / 255.0F;
+                        this.green = ClickGuiModule.INSTANCE.green.getValue().intValue() / 255.0F;
+                        this.blue = ClickGuiModule.INSTANCE.blue.getValue().intValue() / 255.0F;
                         GlStateManager.pushMatrix();
                         GL11.glPushAttrib(1048575);
                         GL11.glPolygonMode(1032, 6913);
@@ -157,9 +131,9 @@ public abstract class MixinRenderLivingBase<T extends EntityLivingBase> extends 
                         GL11.glEnable(3042);
                         GL11.glBlendFunc(770, 771);
                         if (claudius.friendManager.isFriend(entity.getName()) || entity == (Minecraft.getMinecraft()).player) {
-                            GL11.glColor4f(0.0F, 191.0F, 255.0F, (Wireframe.INSTANCE).alpha.getValue().floatValue() / 255.0F);
+                            GL11.glColor4f(0.0F, 191.0F, 255.0F, (Wireframe.getINSTANCE()).alpha.getValue().floatValue() / 255.0F);
                         }
-                        GL11.glLineWidth((Wireframe.INSTANCE).lineWidth.getValue().floatValue());
+                        GL11.glLineWidth((Wireframe.getINSTANCE()).lineWidth.getValue().floatValue());
                         renderModel(entity, f6, f5, f8, f2, f7, f4);
                         GL11.glEnable(2896);
                         GlStateManager.popAttrib();
--- src/main/java/me/crimp/claudius/mod/modules/render/Wireframe.java
+++ src/main/java/me/crimp/claudius/mod/modules/render/Wireframe.java
@@ -5,18 +5,29 @@ import me.crimp.claudius.mod.setting.Setting;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class Wireframe extends Module {
-    public final Setting<Float> alpha = register(new Setting<Float>("PAlpha", 255.0f, 0.1f, 255.0f));
-    public final Setting<Float> lineWidth = this.register(new Setting<Float>("PLineWidth", 1.0f, 0.1f, 3.0f));
-    public Setting<RenderMode> mode = this.register(new Setting<RenderMode>("PMode", RenderMode.SOLID));
+public class Wireframe
+        extends Module {
+    private static Wireframe INSTANCE = new Wireframe();
+    public final Setting<Float> alpha = this.register(new Setting<Float>("PAlpha", Float.valueOf(255.0f), Float.valueOf(0.1f), Float.valueOf(255.0f)));
+    public final Setting<Float> lineWidth = this.register(new Setting<Float>("PLineWidth", Float.valueOf(1.0f), Float.valueOf(0.1f), Float.valueOf(3.0f)));
+    public Setting<RenderMode> mode = this.register(new Setting<RenderMode>("PMode", RenderMode.WIREFRAME));
     public Setting<Boolean> players = this.register(new Setting<Boolean>("Players", Boolean.FALSE));
     public Setting<Boolean> playerModel = this.register(new Setting<Boolean>("PlayerModel", Boolean.FALSE));
-
     public Wireframe() {
         super("Wireframe", "Draws a wireframe esp around other players.", Category.Render, false, false);
+        this.setInstance();
     }
 
-    public static Wireframe INSTANCE = new Wireframe();
+    public static Wireframe getINSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new Wireframe();
+        }
+        return INSTANCE;
+    }
+
+    private void setInstance() {
+        INSTANCE = this;
+    }
 
     @SubscribeEvent
     public void onRenderPlayerEvent(RenderPlayerEvent.Pre event) {
@@ -24,7 +35,6 @@ public class Wireframe extends Module {
     }
 
     public enum RenderMode {
-        SOLID,
         WIREFRAME
 
     }
